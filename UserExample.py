1bb0e0c59c63a1bade2d805e25b3cf378002ff84
3toe <AnthonyJS@gmail.com>
2021-08-26 18:22:25 -0700

    see readme for updates

--- UserExample.py
+++ UserExample.py
@@ -7,19 +7,23 @@ class User:
 
    def make_deposit(self, amount):
       self.account_balance += amount
+      return self
 
 # Add a make_withdrawal method to the User class
    def make_withdrawal(self, amount):
       self.account_balance -= amount
+      return self
 
 # Add a display_user_balance method to the User class
    def display_balance(self):
       print(f"User: {self.name}, Balance: {self.account_balance}")
+      return self
 
 # Transfer money method
    def transfer(self, amount, recipient):
       self.make_withdrawal(amount)
       recipient.make_deposit(amount)
+      return self
 
 # Create 3 instances of the User class   
 Jaylen = User("Jaylen", "JBrown@Celtics.com", 4000000)
@@ -32,6 +36,8 @@ Jaylen.make_deposit(500)
 Jaylen.make_deposit(5000)
 Jaylen.make_withdrawal(5000)
 Jaylen.display_balance()
+# Update with chaining:
+Jaylen.make_deposit(50).make_deposit(500).make_deposit(5000).make_withdrawal(5000).display_balance()
 
 # Have the second user make 2 deposits and 2 withdrawals and then display their balance
 Marcus.make_deposit(50)
@@ -39,6 +45,8 @@ Marcus.make_deposit(500)
 Marcus.make_withdrawal(5000)
 Marcus.make_withdrawal(1000)
 Marcus.display_balance()
+# Update with chaining:
+Marcus.make_deposit(50).make_deposit(500).make_withdrawal(5000).make_withdrawal(1000).display_balance()
 
 # Have the third user make 1 deposits and 3 withdrawals and then display their balance
 Jayson.make_deposit(500)
@@ -46,8 +54,13 @@ Jayson.make_withdrawal(1000)
 Jayson.make_withdrawal(5000)
 Jayson.make_withdrawal(10000)
 Jayson.display_balance()
+# Update with chaining:
+Jayson.make_deposit(500).make_withdrawal(1000).make_withdrawal(5000).make_withdrawal(10000).display_balance()
 
 # BONUS: Add a transfer_money method; have the first user transfer money to the third user and then print both users' balances
 Jaylen.transfer(500, Jayson)
 Jaylen.display_balance()
+Jayson.display_balance()
+# Update with chaining:
+Jaylen.transfer(500, Jayson).display_balance()
 Jayson.display_balance()
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-The first exercise in OOP python, concerning classes, objects and creating attributes and methods therein.
+The first Bootcamp exercise in OOP python, concerning classes, objects and creating attributes and methods therein.
+
+Edit for the second part of the assignment: 
+ 1 updated the methods to "return self"
+ 2 updated the calls with chaining.
